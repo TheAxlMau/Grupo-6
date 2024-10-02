c06c2f262b1c2c2ba65db61cc130e499e17e4d29
Maximo28 <maxjhunior2801@gmail.com>
2024-10-02 02:01:52 -0500

    Cracion de Main

---
+++ Main1/Main1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Main1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttnCambiarTexto = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bttnCambiarTexto
+            // 
+            this.bttnCambiarTexto.Location = new System.Drawing.Point(98, 142);
+            this.bttnCambiarTexto.Name = "bttnCambiarTexto";
+            this.bttnCambiarTexto.Size = new System.Drawing.Size(131, 66);
+            this.bttnCambiarTexto.TabIndex = 0;
+            this.bttnCambiarTexto.Text = "Cambiar Texto";
+            this.bttnCambiarTexto.UseVisualStyleBackColor = true;
+            this.bttnCambiarTexto.Click += new System.EventHandler(this.bttnCambiarTexto_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(373, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 474);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bttnCambiarTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttnCambiarTexto;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
